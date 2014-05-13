41a04ff918898fe7a6070355001e4646215b9b67
nuhuxi <mark.keckeis@gmail.com>
2014-05-13 15:37:32 -0700

    Contains top code but the two are not integrated

--- mapthatworks.js
+++ mapthatworks.js
@@ -1,16 +1,56 @@
 $(document).ready(function() {
         var map;
         function initialize() {
-              var mapOptions = {
-                zoom: 15,
-                center: new google.maps.LatLng(33.613462, -111.954624),
-                mapTypeId: google.maps.MapTypeId.SATELLITE
-              };
-              map = new google.maps.Map(document.getElementById('map-canvas'),
-                  mapOptions);
-            };
+            var mapOptions = {
+            zoom: 15,
+            center: new google.maps.LatLng(33.613462, -111.954624),
+            mapTypeId: google.maps.MapTypeId.SATELLITE
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+        };
 
-        google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, 'load', initialize);
+    
+    $('#getGPS').click( function(event){
+
+        street = $('#streetAddress').val()+'+';
+        city = $('#city').val()+'+';
+        state = $('#state').val();
+
+        // the parameters we need to pass in our request to Google Geocode's API
+        googleURL1 = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+        googleURL2 = '&sensor=true&key=AIzaSyBJh8OQe2-Ni97hUnshrLde2CnkYG4TQTQ';
+        
+        var request =
+            googleURL1+
+            street+
+            city+
+            state+
+            googleURL2;
+
+        var google_response = $.ajax({
+            url: request,
+            type: "GET"})
+            .done(function (data) {
+                $('#latitude').html(" "+data.results[0].geometry.location.lat);
+                $('#longitude').html(" "+data.results[0].geometry.location.lng);
+
+            });
+    });
+    
+    $('#clearForm').click( function(event){
+        $("#streetAddress").val("");
+        $("#city").val("");
+        $("#state").val("");
+        $('#latitude').html("");
+        $('#longitude').html("");
+        
+    });
+    
+
+    $('form').submit(function(e){e.preventDefault();});
+            
     
 });
     
