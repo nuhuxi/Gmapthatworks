dd21ccd2f2313d9c3330c3a36b9e9a256a473e0f
nuhuxi <mark.keckeis@gmail.com>
2014-05-13 17:35:02 -0700

    140513 1734

--- mapthatworks.js
+++ mapthatworks.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         function initialize() {
             var mapOptions = {
             zoom: 15,
-            center: new google.maps.LatLng(33.613462, -111.954624),
+            center: new google.maps.LatLng(38.8978378, -77.0365123),
             mapTypeId: google.maps.MapTypeId.SATELLITE
         };
         map = new google.maps.Map(document.getElementById('map-canvas'),
